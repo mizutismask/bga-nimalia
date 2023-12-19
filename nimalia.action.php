4e4dfe870c87606a4696d3d87dc1b09f3e3e0d8a
mizutismask <severinek@gmail.com>
2023-12-19 18:09:08 +0100

    feat: add copyright

--- nimalia.action.php
+++ nimalia.action.php
@@ -3,7 +3,7 @@
 /**
  *------
  * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
- * Nimalia implementation : © <Your name here> <Your email address here>
+ * Nimalia implementation : © Séverine Kamycki <mizutismask@gmail.com>
  *
  * This code has been produced on the BGA studio platform for use on https://boardgamearena.com.
  * See http://en.doc.boardgamearena.com/Studio for more information.
